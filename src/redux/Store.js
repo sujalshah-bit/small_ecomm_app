21a77615d9723df46475d444e15bfa944f4cc6c2
sujal shah <sujalshah28092004@gmail.com>
2023-03-23 22:15:53 +0530

    name change

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore as createStore} from 'redux'
-import { reducers } from "./reducers";
+import { reducers } from "./Reducers/combine";
 
 const store  = createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
